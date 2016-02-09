835e1177a93f6253e8f81460ded834905821706e
karppa1 <K2264@student.jamk.fi>
2016-02-10 00:09:06 +0200

    Demo3Task5 version 0.01.

---
+++ Demo3Task4/Demo3Task5/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo3Task5
+{
+    class Student
+    {
+        public string Name { get; set; }
+        public int StudentID { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }        
+
+/*        public void PrintData()
+        {
+            Console.WriteLine(ToString());
+            Console.WriteLine();
+        }
+        */
+        public override string ToString()
+        {
+            return ("StudentID: " + StudentID + "\nName:" + Name + "\nAddress: " + Address + "\nPhone: " + Phone);
+        }
+
+
+    }
+}
