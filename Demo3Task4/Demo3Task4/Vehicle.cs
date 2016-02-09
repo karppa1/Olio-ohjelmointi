724fadaf02692723aacec3e960c3cee9daa24110
karppa1 <K2264@student.jamk.fi>
2016-02-09 21:50:38 +0200

    Demo3Task4 version 1.0.

---
+++ Demo3Task4/Demo3Task4/Vehicle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo3Task4
+{
+    class Vehicle
+    {
+        public string Name { get; set; }
+        public int Speed { get; set; }
+        public int Tyres { get; set; }
+
+        public void RemoveData()
+        {
+            Name = "";
+            Speed = 0;
+            Tyres = 0;
+            Console.WriteLine("Data removed!");
+        }
+
+        public void PrintData()
+        {
+            Console.WriteLine(ToString());
+            Console.WriteLine();
+        }
+
+        public override string ToString()
+        {
+            return ("Car: " + Name + " Speed: " + Speed + " Tyres: " + Tyres);
+        }
+    }
+}
