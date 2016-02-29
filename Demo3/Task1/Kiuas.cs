9ef0af6ba637ed2af4ba01d39451a8fb87cae9e8
karppa1 <K2264@student.jamk.fi>
2016-02-29 16:17:55 +0200

    Task1 version 1.0.

---
+++ Demo3/Task1/Kiuas.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Kiuas
+    {
+        public bool Power { get; set; }
+        public double Lampotila { get; set; }
+        public double Kosteus { get; set; }
+
+        public Kiuas(bool power, double lampotila, double kosteus)
+        {
+            Power = power;
+            Lampotila = lampotila;
+            Kosteus = kosteus;
+        }
+
+        public void onOff()
+        {
+            Console.WriteLine("\nLaitetaanko kiuas päälle (K/E)?");
+            string line = Console.ReadLine();
+
+            int number = 0;
+            bool result1 = int.TryParse(line, out number);
+            if (result1)
+            {
+                Console.WriteLine("Error! Et vastannut K tai E! Ohjelma lopetetaan!");
+                return;
+            }
+            char vastaus;
+            bool result2 = char.TryParse(line, out vastaus);
+            if (result2)
+            {
+                switch (vastaus)
+                {
+                    case 'k':
+                    case 'K': Power = true;
+                        Console.WriteLine("Kiuas on päällä");
+                        break;
+                    case 'e':
+                    case 'E': Power = false;
+                        break;
+                    default: Console.WriteLine("Error! Et vastannut K tai E! Ohjelma lopetetaan!");
+                        break;
+                }
+            }
+            
+        }
+
+        public void saadaLampotila()
+        {
+            Console.WriteLine("Aseta haluttu lämpötila:");
+            string line = Console.ReadLine();
+
+            double number = 0.0;
+            bool result2 = double.TryParse(line, out number);
+            if (result2)
+            {
+                Lampotila = number;
+            }
+
+            else
+            {
+                Console.WriteLine("Virheellinen lämpötila!\n");
+            }
+        }
+
+        public void saadaKosteus()
+        {
+            Console.WriteLine("Aseta haluttu kosteus:");
+            string line = Console.ReadLine();
+
+            double number = 0.0;
+            bool result2 = double.TryParse(line, out number);
+            if (result2)
+            {
+                Kosteus = number;
+            }
+
+            else
+            {
+                Console.WriteLine("Virheellinen kosteus!\n");
+            }
+        }
+
+        public void printData()
+        {
+            string tila = "";
+            if (Power == true)
+            {
+                tila = "ON";
+            }
+            else
+            {
+                tila = "OFF";
+            }
+            Console.WriteLine("Kiukaan tiedot:");
+            Console.WriteLine("Virta: " + tila);
+            Console.WriteLine("Lämpötila: " + Lampotila + " astetta");
+            Console.WriteLine("Kosteus: " + Kosteus + " %");
+        }
+    }
+}
---
+++ Demo3/Task1/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kiuas kiuas = new Kiuas(false,0.0,0.0);
+
+            kiuas.printData();
+
+            kiuas.onOff();
+
+            while (kiuas.Power == true)
+            {
+                kiuas.saadaLampotila();
+                kiuas.saadaKosteus();
+                kiuas.printData();
+
+                Console.WriteLine("\nJatketaanko kiukaan säätämistä (K/E)?");
+                string line = Console.ReadLine();
+                Console.WriteLine();
+
+                int number = 0;
+                bool result1 = int.TryParse(line, out number);
+                if (result1)
+                {
+                    Console.WriteLine("Error! Et vastannut K tai E! Ohjelma lopetetaan!");
+                    return;
+                }
+                char vastaus;
+                bool result2 = char.TryParse(line, out vastaus);
+                if (result2)
+                {
+                    switch (vastaus)
+                    {
+                        case 'k':
+                        case 'K':
+                            kiuas.Power = true;
+                            break;
+                        case 'e':
+                        case 'E':
+                            kiuas.Power = false;
+                            break;
+                        default:
+                            Console.WriteLine("Error! Et vastannut K tai E! Ohjelma lopetetaan!");
+                            break;
+                    }
+                }
+            }
+            if (kiuas.Power == false)
+            {
+                Console.WriteLine("Kiuas on pois päältä\n");
+                kiuas.Lampotila = 0.0;
+                kiuas.Kosteus = 0.0;
+                kiuas.printData();
+            }
+            
+
+            Console.ReadLine();
+        }
+    }
+}
