d61175ea3847fa129a908f90ca57995d0ef12341
karppa1 <K2264@student.jamk.fi>
2016-03-02 23:09:34 +0200

    Task5 version 1.0.

---
+++ Demo3/Task5/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int MaxStudents = 5;
+            Student[] students = new Student[MaxStudents];
+
+            Student student1 = new Student(1234, "Aku","Ankkalinna 1", "040-12345678");
+            students[0] = student1;
+
+            Student student2 = new Student(2353, "Hessu", "Ankkalinna 2", "040-12345443");
+            students[1] = student2;
+
+            Student student3 = new Student(5432,"Roope","Ankkalinna 3","040-11112222");
+            students[2] = student3;
+
+            Student student4 = new Student(2345,"Hannu","Ankkalinna 4","040-11231244");
+            students[3] = student4;
+
+            Student student5 = new Student(3324,"Mikki","Ankkalinna 5","040-12334525");
+            students[4] = student5;
+
+            for (int i = 0; i < MaxStudents; i++)
+            {
+                Console.WriteLine(students[i]);
+                Console.WriteLine();
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Demo3/Task5/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5
+{
+    class Student
+    {
+        public int StudentID { get; set; }
+        public string Name { get; set; }        
+        public string Address { get; set; }
+        public string Phone { get; set; }
+
+        public Student(int studentId, string name,string address,string phone)
+        {
+            StudentID = studentId;
+            Name = name;            
+            Address = address;
+            Phone = phone;
+        }
+
+        public override string ToString()
+        {
+            return ("StudentID: " + StudentID + "\nName:" + Name + "\nAddress: " + Address + "\nPhone: " + Phone);
+        }
+    }
+}
