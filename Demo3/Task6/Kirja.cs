e0f7008c1220e5234d65a98022543cf16bd20870
karppa1 <K2264@student.jamk.fi>
2016-03-03 01:30:23 +0200

    Task6 version 1.0.

---
+++ Demo3/Task6/Kirja.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6
+{
+    class Kirja
+    {
+        public string Nimi { get; set; }
+        public string Kirjailija { get; set; }
+        public double Hinta { get; set; }
+        public int Painos { get; set; }
+
+        public Kirja(string nimi,string kirjailija,double hinta,int painos)
+        {
+            Nimi = nimi;
+            Kirjailija = kirjailija;
+            Hinta = hinta;
+            Painos = painos;
+        }
+
+        public override string ToString()
+        {
+            return "Nimi: " + Nimi + "\nKirjailija: " + Kirjailija + "\nHinta: " + Hinta + " euroa" + "\nPainos: " + Painos + ".painos";
+        }
+    }
+}
---
+++ Demo3/Task6/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int maxKirjaa = 5;
+            Kirja[] kirjat = new Kirja[maxKirjaa];
+
+            Kirja kirja1 = new Kirja("Olio-ohjelmointi", "Aku Ankka", 39.95, 8);
+            kirjat[0] = kirja1;
+
+            Kirja kirja2 = new Kirja("SQL", "Hessu Hopo", 29.75, 2);
+            kirjat[1] = kirja2;
+
+            Kirja kirja3 = new Kirja("C++", "Hannu Hanhi", 25.85, 1);
+            kirjat[2] = kirja3;
+
+            Kirja kirja4 = new Kirja("3D-mallinnus", "Mikki Hiiri", 24.15, 4);
+            kirjat[3] = kirja4;
+
+            Kirja kirja5 = new Kirja("Peliohjelmointi", "Pelle Peloton", 49.75, 2);
+            kirjat[4] = kirja5;
+
+            Console.WriteLine("* Myynnissä olevat kirjat *");
+            Console.WriteLine("*************************");
+            for(int i = 0; i < maxKirjaa; i++)
+            {
+                Console.WriteLine(kirjat[i]);
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
