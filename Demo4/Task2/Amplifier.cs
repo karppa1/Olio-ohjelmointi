a3af8eb0ccc8d984783e42abbe7e602aadae5c02
karppa1 <K2264@student.jamk.fi>
2016-03-04 00:55:38 +0200

    Task2 version 1.0.

---
+++ Demo4/Task2/Amplifier.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Amplifier
+    {
+        public int Volume
+        {
+            get { return volume; }
+            set
+            {
+                if (value < 0)
+                {
+                    volume = 0;
+                    Console.WriteLine("Too low volume - Amplifier volume is set to minimum : " + volume);
+                }
+                else if (value > 100)
+                {
+                    volume = 100;
+                    Console.WriteLine("Too much volume -  Amplifier volume is set to maximum : " + volume);
+                }
+                else volume = value;
+            }
+        }
+
+        public void askNumber()
+        {
+            
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Give a new volume value (0-100) >");
+                string line = Console.ReadLine();
+
+                int number = 0;
+                bool result = int.TryParse(line, out number);
+                if (result)
+                {
+                    Volume = number;
+                }
+                else
+                {
+                    Console.WriteLine("Error! You didn't gave a number!");
+                    return;
+                }
+
+                printData();
+            }
+        }
+
+        public void printData()
+        {
+            Console.WriteLine("Amplifier volume is set to : " + volume);
+        }
+
+        private int volume = 0;
+    }
+}
---
+++ Demo4/Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Amplifier amplifier = new Amplifier();
+            amplifier.printData();
+
+            amplifier.askNumber();
+
+            Console.ReadLine();
+        }
+    }
+}
