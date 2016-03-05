0715a8b0ec65a4c6cdb56a9ff8cae96ad09fbd15
karppa1 <K2264@student.jamk.fi>
2016-03-05 15:15:41 +0200

    Task5 version 0.01.

---
+++ Demo4/Task5/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Radio radio = new Radio(false, 0, 0.0f);
+            radio.printData();
+
+                Console.WriteLine("Laitetaanko radio päälle (K/E)?");   // Kesken...
+                string line = Console.ReadLine();
+
+                int number = 0;
+                bool result = int.TryParse(line, out number);
+                if (result)
+                {
+                    Console.WriteLine("Error! Et vastannut K tai E!");
+                    radio.Power = false;
+                }
+                else radio.Power = true;
+
+                while (radio.Power == true)
+                {
+                    radio.adjustVolume();
+                    radio.adjustFrequency();
+                    radio.printData();
+
+                Console.WriteLine("Jatketaanko radion säätämistä (K/E)?");  // Kesken...
+                string line2 = Console.ReadLine();
+
+                int number2 = 0;
+                bool result2 = int.TryParse(line, out number2);
+                if (result2)
+                {
+                    Console.WriteLine("Error! Et vastannut K tai E!");
+                    radio.Power = false;
+                }
+                else radio.Power = true;
+            }
+        }
+    }
+}
---
+++ Demo4/Task5/Radio.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5
+{
+    class Radio
+    {
+        public bool Power { get; set; }
+        public int Volume
+        {
+            get { return volume; }
+            set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("Error! Väärä arvo!");
+                    volume = 0;
+                }
+                else if (value > 9)
+                {
+                    Console.WriteLine("Error! Väärä arvo!");
+                    volume = 9;
+                }
+                else volume = value;
+            }
+        }
+
+        public float Frequency
+        {
+            get { return frequency; }
+            set
+            {
+                if (value < 2000.0f)
+                {
+                    frequency = 2000.0f;
+                }
+                else if (value > 26000.0f)
+                {
+                    frequency = 26000.0f;
+                }
+                else frequency = value;
+            }
+        }
+
+        public Radio(bool power, int volume, float frequency)
+        {
+            Power = power;
+            Volume = volume;
+            Frequency = frequency;
+        }
+
+        public void onOff()
+        {
+            Power = true;
+        }
+
+        public void adjustVolume()
+        {
+            Console.WriteLine("\nSäädä äänenvoimakkuus (0-9) > ");
+            string line = Console.ReadLine();
+
+            int number = 0;
+            bool result = int.TryParse(line, out number);
+            if (result)
+            {
+                volume = number;
+            }
+            else Console.WriteLine("Error!");
+        }
+
+        public void adjustFrequency()
+        {
+            Console.WriteLine("\nSäädä taajuus (2000.0 - 26000.0) > ");
+            string line = Console.ReadLine();
+
+            float number = 0.0f;
+            bool result = float.TryParse(line, out number);
+            if (result)
+            {
+                frequency = number;
+            }
+            else Console.WriteLine("Error!");
+        }
+
+        public override string ToString()
+        {
+            return "\nPower: " + tila + "\nVolume: " + Volume + "\nFrequncy: " + Frequency;
+        }
+
+        public void printData()
+        {
+            if (Power == true)
+            {
+                tila = "ON";
+            }
+            else tila = "OFF";
+
+            Console.WriteLine("Radion tila:");
+            Console.WriteLine(ToString());
+        }
+
+        private int volume;
+        private float frequency;
+        private string tila = "";
+    }
+}
