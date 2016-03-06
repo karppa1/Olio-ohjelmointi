5411b58e4c5a7196f60cf5f8a4898ff21bc62f57
karppa1 <K2264@student.jamk.fi>
2016-03-06 20:32:12 +0200

    Task1 version 1.0.

--- Demo7/Task1/Program.cs
+++ Demo7/Task1/Program.cs
@@ -3,28 +3,59 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Task1
 {
     class Program
     {
         static void Main(string[] args)
-        {
-            bool tila = true;                           // Kesken...
+        {            
             List<string> teksti = new List<string>();
+            
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.WriteLine("Kirjoita merkkijono > ");
+                    string line = Console.ReadLine();
+                    
+                    teksti.Add(line);
+                }
 
-            for(int i = 0; i < 5; i++)
-            {
-                Console.WriteLine("Kirjoita merkkijono > ");
-                string line = Console.ReadLine();
-
-                teksti.Add(line);
-            }
-
-            Console.WriteLine();
+            string rivit = "";
             foreach(string data in teksti)
             {
-                Console.WriteLine(data);
+                rivit += "\n" + data;
+            }
+
+            // Tallennetaan tiedostoon...
+            System.IO.StreamWriter outputFile = null;
+
+            try
+            {
+                outputFile = new System.IO.StreamWriter("tekstit.txt");
+                outputFile.WriteLine(rivit);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {                
+                if (outputFile != null)
+                {
+                    outputFile.Close();
+                }
+            }
+
+            // Luetaan tiedoston sisältö ja tulostetaan se näkyviin
+            try
+            {
+                string sisalto = System.IO.File.ReadAllText("tekstit.txt");
+                System.Console.WriteLine("Tiedoston sisältö:\n" + sisalto);
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("File not found (FileNotFoundException)");
             }
 
             Console.ReadLine();
