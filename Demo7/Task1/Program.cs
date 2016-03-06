44e226fb6d5188edf971d684611600a5721a32cd
karppa1 <K2264@student.jamk.fi>
2016-03-06 03:12:26 +0200

    Task1 version 0.01.

---
+++ Demo7/Task1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool tila = true;                           // Kesken...
+            List<string> teksti = new List<string>();
+
+            for(int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Kirjoita merkkijono > ");
+                string line = Console.ReadLine();
+
+                teksti.Add(line);
+            }
+
+            Console.WriteLine();
+            foreach(string data in teksti)
+            {
+                Console.WriteLine(data);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
