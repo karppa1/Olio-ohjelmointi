d67b5761759674f6c4b25ac64919c84f8e94e30b
karppa1 <K2264@student.jamk.fi>
2016-03-09 00:45:38 +0200

    Task1 version 0.01.

---
+++ Demo8/Task1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int number;
+
+            Console.WriteLine("How many times you want to throw a dice? > ");
+            string line = Console.ReadLine();
+            bool result = int.TryParse(line, out number);
+
+            int sum = 0;
+
+            if (result)
+            {
+                for (int i = 0; i < number; i++)
+                {
+                    int dice = rand.Next(1, 7);
+
+                    //Console.WriteLine(dice);
+                    sum = +(sum + dice);
+                }
+            }
+            else {
+                Console.WriteLine("Error!");
+                number = 1;
+            }
+
+            double avg = 0.0f;
+            avg = (sum / (double)number);
+
+            Console.WriteLine("Average: {0:F2}", avg);
+
+            Console.ReadLine();
+        }
+    }
+}
