bf1e9f53980dd81e0866c11fd383006e3d4a910a
karppa1 <K2264@student.jamk.fi>
2016-03-08 04:48:41 +0200

    Task4 version 0.01.

---
+++ Demo10/Task4/Kiuas.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class Kiuas
+    {
+        public int Temperature
+        {
+            get { return temperature; }
+            set
+            {
+                if (value < 0) temperature = 0;
+                if (value > 120) temperature = 120;
+                else temperature = value;
+            }
+        }
+        public int Humidity
+        {
+            get { return humidity; }
+            set
+            {
+                if (value < 0) humidity = 0;
+                if (value > 100) humidity = 100;
+                else humidity = value;
+            }
+        }
+
+        private int temperature;
+        private int humidity;
+    }
+}
