db3bdf0e511ac7be38a66707527106b05e95ff85
karppa1 <K2264@student.jamk.fi>
2016-03-09 01:10:56 +0200

    Task2 version 1.0.

---
+++ Demo8/Task2/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Product
+    {
+        public string Tuote { get; set; }
+        public double Hinta { get; set; }
+
+        public Product(string tuote, double hinta)
+        {
+            Tuote = tuote;
+            Hinta = hinta;
+        }
+
+        public override string ToString()
+        {
+            return " - product: " + Tuote + " " + Hinta + "e";
+        }
+    }
+}
---
+++ Demo8/Task2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> products = new List<string>();
+
+            Product product1 = new Product("Milk", 1.4);
+            Product product2 = new Product("Beer", 2.2);
+            Product product3 = new Product("Butter", 3.2);
+            Product product4 = new Product("Cheese", 4.2);
+
+            products.Add(product1.ToString());
+            products.Add(product2.ToString());
+            products.Add(product3.ToString());
+            products.Add(product4.ToString());
+            
+            Console.WriteLine("All products in collection: \n");            
+            foreach(string data in products)
+            {
+                Console.WriteLine(data);
+            }            
+
+            Console.ReadLine();
+        }
+    }
+}
