8932814a887fcce1539c3896578927f673f5eb80
karppa1 <K2264@student.jamk.fi>
2016-03-08 03:56:54 +0200

    Task3 version 1.0.

---
+++ Demo10/Task3/Lotto.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Lotto
+    {
+        private Random rand = new Random();
+
+        public List<int> arvotutNumerot(string peli)
+        {
+            int maxArvo = 0;    // Suurin sallittu arvottava luku
+            int maxNumerot = 0; // Arvottavien numeroiden määrä
+
+            if (peli.Equals("Lotto"))
+            {
+                maxArvo = 39;
+                maxNumerot = 7;
+            }
+            else if(peli.Equals("Viking Lotto"))
+            {
+                maxArvo = 48;
+                maxNumerot = 6;
+            }
+            else if (peli.Equals("Eurojackpot"))
+            {
+                maxArvo = 50;
+                maxNumerot = 5;
+            }
+
+            List<int> numerot = new List<int>();
+            do
+            {
+                int numero = rand.Next(1, maxArvo + 1);
+
+                // Tarkistus löytyykö arvottu numero jo listalta, jos ei lisätään listaan...
+                if (!numerot.Contains(numero)) numerot.Add(numero);
+
+            } while (numerot.Count < maxNumerot);
+
+            numerot.Sort();
+
+            if (peli.Equals("Eurojackpot"))
+            {
+                List<int> tahtiNumerot = new List<int>();
+                do
+                {
+                    int numero = rand.Next(1, 11);
+
+                    // Tarkistus löytyykö arvottu numero jo listalta, jos ei lisätään listaan...
+                    if (!tahtiNumerot.Contains(numero)) tahtiNumerot.Add(numero);
+
+                } while (tahtiNumerot.Count < 2);
+
+                tahtiNumerot.Sort();
+                numerot.AddRange(tahtiNumerot);
+            }
+
+            return numerot;
+        }
+    }
+}
---
+++ Demo10/Task3/MainPage.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Task3
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private Lotto lotto;
+        private string peli;
+        private int rivit;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            lotto = new Lotto();
+        }      
+
+        private void button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+            int riviMaara;
+            bool result = int.TryParse(textBox.Text, out riviMaara);
+            if (result)
+            {
+                rivit = riviMaara;
+            }
+
+            else
+            {
+                rivit = 0;
+            }
+
+            string arvotturivi = "";
+
+            for (int i = 1; i <= rivit; i++)
+            {
+                List<int> numerot = lotto.arvotutNumerot(peli);
+                arvotturivi += "Rivi " + i + ": ";
+
+                for (int j = 0; j < numerot.Count; j++)
+                {
+                    arvotturivi += numerot[j] + " ";
+                }
+                arvotturivi += " \n";
+            }
+
+            textBlock3.Text = arvotturivi;
+        }
+
+        private void comboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            peli = comboBox.SelectedValue.ToString();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            textBlock3.Text = "";
+        }
+
+    }
+}
